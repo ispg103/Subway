f7f1fc9988fc01f4df6c6e8e043e432b818b2819
Your Name <you@example.com>
2023-10-19 20:56:56 -0500

    adding to bread

--- screens/bread.js
+++ screens/bread.js
@@ -6,15 +6,17 @@ export class Bread{
       this.oregano =this.p5.loadImage('../SUBWAY FOTOS/BREAD/OreganoParmesan.png');
       this.wheat =this.p5.loadImage('../SUBWAY FOTOS/BREAD/WholeWheat.png');
       this.choose =this.p5.loadImage('../SUBWAY FOTOS/TITLES/choose.png');
+      this.bread =this.p5.loadImage('../SUBWAY FOTOS/TITLES/Bread.png');
 
     }
     show(p5){
         p5.background('green');
         p5.image(this.logo,65,75);
         p5.image(this.choose,55,170);
-        p5.image(this.white,90,320);
-        p5.image(this.oregano,85,430);
-        p5.image(this.wheat,85,530);
+        p5.image(this.white,90,330);
+        p5.image(this.oregano,85,440);
+        p5.image(this.wheat,85,540);
+        p5.image(this.bread,150,280);
     }
 
     hideInput(){
--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let userInfo;
 let bread;
 
 const app = p5 => {
-  const userInfo = new UserInfo(p5);
+  const bread = new Bread(p5);
 
   p5.setup = function() {
     p5.createCanvas(393, 715);
@@ -14,7 +14,7 @@ const app = p5 => {
 
   p5.draw = function() {
     p5.background(220);
-    userInfo.show(p5);
+    bread.show(p5);
   };
 
 };
