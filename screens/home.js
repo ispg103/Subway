015e25723ae82fbb0cc4d2081f71e0c08977bd5b
Your Name <you@example.com>
2023-10-19 22:44:09 -0500

    home

--- screens/home.js
+++ screens/home.js
@@ -3,17 +3,19 @@ export class Home{
       this.p5=p5;
       this.logo =this.p5.loadImage('../SUBWAY FOTOS/EXTRAS/SubwayLogo.png');
       this.icon=this.p5.loadImage('../SUBWAY FOTOS/EXTRAS/SubwayIcon.png');
+      this.build=this.p5.loadImage('../SUBWAY FOTOS/TITLES/build.png');
 
         this.startButton = this.p5.createButton('Start');
-        this.startButton.position(this.p5.windowWidth / 2 - 50, this.p5.windowHeight / 2 + 60);
+        this.startButton.position(this.p5.windowWidth / 7.5 - 50, this.p5.windowHeight / 2 + 250);
         this.startButton.mousePressed(this.navigate);
 
         this.hideInput();
     }
     show(p5){
         p5.background('green');
-        p5.image(this.logo,40,100);
-        p5.image(this.icon,40,100);
+        p5.image(this.logo,65,85);
+        p5.image(this.icon,100,200);
+        p5.image(this.build,90,470);
       this.startButton.show();
 
     }
