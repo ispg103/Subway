27f777e06e05282cfb34f329ed904af43ee7adaf
Your Name <you@example.com>
2023-10-20 19:46:31 -0500

    fixing things

--- screens/meats.js
+++ screens/meats.js
@@ -18,27 +18,37 @@ export class Meats{
       this.meat =this.p5.loadImage('../SUBWAY FOTOS/TITLES/Meat.png');
 
       this.nextButton = this.p5.createButton('Next');
-      this.nextButton.position(this.p5.windowWidth / 7.3 - 50, this.p5.windowHeight / 2 + 300);
+      this.nextButton.position(this.p5.windowWidth / 10 - 87, this.p5.windowHeight / 2 + 275);
       this.nextButton.mousePressed(this.navigate);
 
     }
     show(p5){
         p5.background('green');
-        p5.image(this.logo,65,75);
-        p5.image(this.choose,55,155);
-        p5.image(this.meat,155,265);
-        p5.image(this.jam,55,300);
-        p5.image(this.jamText,92,385);
-        p5.image(this.chicken,190,300);
-        p5.image(this.chickenText,205,385);
-        p5.image(this.turkey,50,405);
-        p5.image(this.turkeyText,85,505);
-        p5.image(this.salami,190,422);
-        p5.image(this.salamiText,230,503);
-        p5.image(this.tuna,50,537);
-        p5.image(this.tunaText,90,622);
-        p5.image(this.ribs,200,537);
-        p5.image(this.ribsText,222,622);
+        p5.image(this.logo,65,60);
+        p5.image(this.choose,55,140);
+        p5.image(this.meat,155,250);
+        p5.image(this.jam,55,275);
+        p5.image(this.jamText,92,363);
+        p5.image(this.chicken,200,275);
+        p5.image(this.chickenText,215,362);
+        p5.image(this.turkey,50,383);
+        p5.image(this.turkeyText,85,484);
+        p5.image(this.salami,200,403);
+        p5.image(this.salamiText,240,482);
+        p5.image(this.tuna,50,515);
+        p5.image(this.tunaText,90,597);
+        p5.image(this.ribs,210,514);
+        p5.image(this.ribsText,230,595);
+
+
+        this.nextButton.style('background-color', '#F3B90C');
+        this.nextButton.style('color', '#003308');
+        this.nextButton.style('font-size', '29px');
+        this.nextButton.style('width', '249px');
+        this.nextButton.style('height', '50px');
+        this.nextButton.style('border-radius', '39.5px');
+        this.nextButton.style('border', 'none');
+        this.nextButton.style('box-shadow', '0px 4px 4px 0px rgba(0, 0, 0, 0.25');
 
     }
 
