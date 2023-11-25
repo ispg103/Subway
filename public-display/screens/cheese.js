68c669916078ea476f88eacf47c3c1c7b8db5f0c
Your Name <you@example.com>
2023-11-25 11:50:28 -0500

    new photos add

---
+++ public-display/screens/cheese.js
@@ -0,0 +1,38 @@
+class CheeseItem {
+    constructor(image, text) {
+      this.image = image;
+      this.text = text;
+    }
+  }
+  
+  export class Cheese {
+    constructor(p5, navigateCallback) {
+      this.p5 = p5;
+      this.logo = this.p5.loadImage('../SUBWAY FOTOS/EXTRAS/SubwayLogo.png');
+      this.choose = this.p5.loadImage('../SUBWAY FOTOS/TITLES/Bread_chosen.png');
+      this.cheese = this.p5.loadImage('../SUBWAY FOTOS/CHEESE/ProvoloneCheese.png');
+
+      //this.next.mousePressed(()=>{
+        //navigateCallback();
+      //});
+  
+      //this.hideInput();
+    }
+  
+    show(p5) {
+      p5.background('green');
+      p5.image(this.logo, 65, 75);
+      p5.image(this.choose, 55, 140);
+      p5.image(this.cheese, 150, 260);
+  
+    }
+  
+  
+    //hideInput() {
+      //this.nextButton.hide();
+    //}
+  
+    //showInput() {
+      //this.nextButton.show();
+    //}
+  }
---
+++ public-display/screens/meats.js
@@ -0,0 +1,38 @@
+class MeatsItem {
+    constructor(image, text) {
+      this.image = image;
+      this.text = text;
+    }
+  }
+  
+  export class Meats {
+    constructor(p5, navigateCallback) {
+      this.p5 = p5;
+      this.logo = this.p5.loadImage('../SUBWAY FOTOS/EXTRAS/SubwayLogo.png');
+      this.choose = this.p5.loadImage('../SUBWAY FOTOS/TITLES/Bread_chosen.png');
+      this.meat = this.p5.loadImage('../SUBWAY FOTOS/MEAT/Jam.png');
+
+      //this.next.mousePressed(()=>{
+        //navigateCallback();
+      //});
+  
+      //this.hideInput();
+    }
+  
+    show(p5) {
+      p5.background('green');
+      p5.image(this.logo, 65, 75);
+      p5.image(this.choose, 55, 140);
+      p5.image(this.meat, 150, 260);
+  
+    }
+  
+  
+    //hideInput() {
+      //this.nextButton.hide();
+    //}
+  
+    //showInput() {
+      //this.nextButton.show();
+    //}
+  }
