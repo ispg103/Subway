c7742902d8dd71bce40d7641b79a57409d9db521
Your Name <you@example.com>
2023-12-01 14:54:07 -0500

    para que tengan sentido las dos pantallas

--- public-controller/sketch.js
+++ public-controller/sketch.js
@@ -80,20 +80,20 @@ const app = (p5) => {
 
     bread = new Bread(p5, () => {
       currentScreen.hideInput();
-      currentScreen = vegetables;
+      currentScreen = meats;
     });
 
-    vegetables = new Vegetables(p5, () => {
+    meats = new Meats(p5, () => {
       currentScreen.hideInput();
       currentScreen = cheese;
     });
 
     cheese = new Cheese(p5, () => {
       currentScreen.hideInput();
-      currentScreen = meats;
+      currentScreen = vegetables;
     });
 
-    meats = new Meats(p5, () => {
+    vegetables = new Vegetables(p5, () => {
       currentScreen.hideInput();
       currentScreen = sauces;
     });
@@ -124,7 +124,7 @@ const app = (p5) => {
     });
 
     // VER LAS PANTALLAS DESDE AQUI
-    currentScreen = home;
+    currentScreen = bread;
 
     `userInfo = new UserInfo(p5, () => {
       userInfo.setSubmitCallback((userData)=>{
